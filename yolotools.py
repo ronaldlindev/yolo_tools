316db7f89bf6bba459453b62061155fefb6e5428
unknown <ronaldlindev@gmail.com>
2024-08-14 09:17:09 +0800

    added zoom functionality to plot

--- yolotools.py
+++ yolotools.py
@@ -1,4 +1,6 @@
 import os
+from pathlib import Path
+
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -29,12 +31,27 @@ def to_xyxy(x, y, w, h):
     y2 = y + h / 2
     return x1, y1, x2, y2
 
+
+## given absolute path to an image, find its corresponding label
+def image_to_label(img_path: str) -> str:
+    p = Path(img_path)
+    ds_path = Path(*p.parts[:-2])
+    img_name = str(p.parts[-1])
+    return ds_path.joinpath('labels', img_name[:-3] + 'txt')
+
+
+def get_labels(self, img_path):
+    label_path = image_to_label(img_path=img_path)
+    with open(label_path) as f:
+        label, x, y, w, h = f.readline().split(" ")[:5]
+        return label, x, y, w, h
+    
     # takes xywh norm 
 def plot(img: np.array, labels: list): 
     image_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert to RGB for matplotlib
 
     # Plot the image and boxes
-    fig, ax = plt.subplots(1, figsize=(12, 9))
+    fig, ax = plt.subplots(1, figsize=(12, 9))      
     ax.imshow(image_rgb)
 
     # Draw boxes
@@ -51,9 +68,49 @@ def plot(img: np.array, labels: list):
     rect = plt.Rectangle((x_left, y_top), true_width,
                         true_height, fill=False, color='red', linewidth=2)
     ax.add_patch(rect)
-
+    
+    fig.canvas.mpl_connect('scroll_event', zoom_and_pan)
+    fig.canvas.mpl_connect('button_press_event', zoom_and_pan)
+    fig.canvas.mpl_connect('motion_notify_event', zoom_and_pan)
+    fig.canvas.mpl_connect('button_release_event', zoom_and_pan)
+    
     plt.axis('off')
     plt.show(block=False)
-    plt.pause(10.0)
+    plt.pause(100.0)
     plt.close()
+    # Add zoom and pan functionality to the plot
+
+pan_start = None
+
+def zoom_and_pan(event):
+    global pan_start
+    axtemp = event.inaxes
+    if axtemp is None:
+        return
+
+    x_min, x_max = axtemp.get_xlim()
+    y_min, y_max = axtemp.get_ylim()
+    x_delta = (x_max - x_min) / 10
+    y_delta = (y_max - y_min) / 10
+
+    if event.name == 'scroll_event':
+        if event.button == 'up':
+            axtemp.set(xlim=(x_min + x_delta, x_max - x_delta))
+            axtemp.set(ylim=(y_min + y_delta, y_max - y_delta))
+        elif event.button == 'down':
+            axtemp.set(xlim=(x_min - x_delta, x_max + x_delta))
+            axtemp.set(ylim=(y_min - y_delta, y_max + y_delta))
+        axtemp.figure.canvas.draw()
+    elif event.name == 'button_press_event' and event.button == 1:
+        pan_start = (event.xdata, event.ydata)
+    elif event.name == 'motion_notify_event' and pan_start:
+        dx = event.xdata - pan_start[0]
+        dy = event.ydata - pan_start[1]
+        axtemp.set_xlim(x_min - dx, x_max - dx)
+        axtemp.set_ylim(y_min - dy, y_max - dy)
+        axtemp.figure.canvas.draw()
+    elif event.name == 'button_release_event' and event.button == 1:
+        pan_start = None
+
+
 
